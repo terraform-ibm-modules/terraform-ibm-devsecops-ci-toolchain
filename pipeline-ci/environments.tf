8db08456a2a6cfeb41adfc3f845354459e40a2bd
HUA YUEN HUI <HUAYUENH@ie.ibm.com>
2023-09-04 15:25:25 +0100

    chore: update doc (#76)

--- pipeline-ci/environments.tf
+++ pipeline-ci/environments.tf
@@ -242,13 +242,6 @@ resource "ibm_cd_tekton_pipeline_property" "ci_pipeline_ibm_cloud_api" {
   pipeline_id = ibm_cd_tekton_pipeline.ci_pipeline_instance.pipeline_id
 }
 
-resource "ibm_cd_tekton_pipeline_property" "ci_pipeline_sonarqube" {
-  name        = "sonarqube"
-  type        = "text"
-  value       = "{}"
-  pipeline_id = ibm_cd_tekton_pipeline.ci_pipeline_instance.pipeline_id
-}
-
 resource "ibm_cd_tekton_pipeline_property" "ci_pipeline_sonarqube-config" {
   name        = "sonarqube-config"
   type        = "text"
