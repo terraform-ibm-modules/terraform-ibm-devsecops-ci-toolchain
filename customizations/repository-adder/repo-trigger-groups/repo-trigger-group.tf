3ba023ba320643e60d8bc0b3f68f4777b154172e
HUA YUEN HUI <HUAYUENH@ie.ibm.com>
2025-09-12 11:07:15 +0100

    fix: allow triggers to target a specific branch (#136)

--- customizations/repository-adder/repo-trigger-groups/repo-trigger-group.tf
+++ customizations/repository-adder/repo-trigger-groups/repo-trigger-group.tf
@@ -60,7 +60,7 @@ locals {
     enabled                  = trigger.enabled
     max_concurrent_runs      = trigger.max_concurrent_runs
     pipeline_id              = trigger.pipeline_id
-    default_branch           = local.default_branch
+    default_branch           = (trigger.default_branch != "") ? trigger.default_branch : local.default_branch
     events                   = trigger.events
     repo_url                 = local.repo_url_raw
     config_data              = var.config_data
