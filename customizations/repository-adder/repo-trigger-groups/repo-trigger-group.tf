129187c164b5d68261dfde842fceabaae5e45135
huayuenh <48723418+huayuenh@users.noreply.github.com>
2025-09-17 12:58:41 +0100

    Region override (#14)
    * fix: allow triggers to target a specific branch (#136)
    
    * feat: add cel filter and trigger pattern support for custom trigger (#137)
    
    * chore: test region override

--- customizations/repository-adder/repo-trigger-groups/repo-trigger-group.tf
+++ customizations/repository-adder/repo-trigger-groups/repo-trigger-group.tf
@@ -60,7 +60,9 @@ locals {
     enabled                  = trigger.enabled
     max_concurrent_runs      = trigger.max_concurrent_runs
     pipeline_id              = trigger.pipeline_id
-    default_branch           = local.default_branch
+    default_branch           = (trigger.default_branch != "") ? trigger.default_branch : local.default_branch
+    pattern                  = trigger.pattern
+    filter                   = trigger.filter
     events                   = trigger.events
     repo_url                 = local.repo_url_raw
     config_data              = var.config_data
